306ff65d63d736d677a69e8e80d64eea1e018f1d
Matthew Gall <me@matthewgall.dev>
2025-01-15 00:17:39 +0000

    Added additional records to Migadu configuration

--- global/functions/migadu.js
+++ global/functions/migadu.js
@@ -1,6 +1,11 @@
 var MIGADU_RECORDS = [
+    CNAME("autoconfig", "autoconfig.migadu.com."),
     MX("@", 10, "aspmx1.migadu.com."),
     MX("@", 20, "aspmx2.migadu.com."),
+    SRV("_autodiscover._tcp", 0, 1, 443, "autodiscover.migadu.com."),
+	SRV("_imaps._tcp", 0, 1, 993, "imap.migadu.com."),
+	SRV("_pop3s._tcp", 0, 1, 995, "pop.migadu.com."),
+    SRV("_submissions._tcp", 0, 1, 465, "smtp.migadu.com.")
 ];
 var MIGADU_DKIM = function(domain){
     return [

e946eb679be1fd05a82cb3cd1ee56dd293e8068b
Matthew Gall <me@matthewgall.dev>
2025-01-15 01:02:24 +0000

    Corrected aviationmuseum.wales to use Zoho EU

--- zones/swam/aviationmuseum.wales.js
+++ zones/swam/aviationmuseum.wales.js
@@ -5,7 +5,7 @@ D("aviationmuseum.wales", REG_NONE, DnsProvider(DNS_CLOUDFLARE_SWAM),
 	MIGADU_DKIM('aviationmuseum.wales'),
     TXT("@", "hosted-email-verify=s9wfcvle"),
     // Zoho
-    ZOHO_MX_RECORDS,
+    ZOHO_EU,RECORDS,
 	TXT("@", "zoho-verification=zb93670420.zmverify.zoho.eu"),
     TXT("zoho1._domainkey", "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1edRqYFgG5FSEj4lsI9+EAsyYK2ae4y5MvA1tC1BJwkfyKCwTx2ziFhbNfWBpwrZDusve3AQW60YVM3gakKCpR7RbOnnA22zHm1nHpIB2f0RTndsbzWoafpYLh7uDeKpjDfAUf898gkFZbpexeMbE0YZrjUjofpJy1o8rqJSTk9VfOY8wAms9FSxLRAWq+FkKHR2IWmCRwZnru8aUKocDxClw7Mz9qpIoVjjKk328wuy8Ai+xxQHPysdm3XDV3CudLigsIdSY3pWHN1ZVGn0i6DXBmE3K2FiEHAoSj7PazkyzmWzaLVDO16msFujqFJxopy3mtXlaRLtUyCPkU3SVQIDAQAB"),
     // DMARC
